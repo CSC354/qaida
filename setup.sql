a86281a2c5e2914cd18b8d6994969a7abd341379
salehmu <ghd@keemail.me>
2022-11-22 02:57:46 +0200

    Init SQL files

---
+++ setup.sql
@@ -0,0 +1,28 @@
+USE master;
+
+IF DB_ID ('SIJL') IS NOT NULL DROP DATABASE SIJL;
+
+IF @@ERROR = 3702
+   RAISERROR ('Database cannot be dropped because there are still open connections.', 127, 127)
+   WITH NOWAIT, LOG;
+
+
+CREATE DATABASE SIJL;
+
+GO
+
+USE SIJL;
+
+GO
+CREATE SCHEMA SIJL AUTHORIZATION dbo;
+GO
+
+CREATE TABLE SIJL.DATA
+(
+    id INT IDENTITY(1,1) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(254) NOT NULL,
+    age smallint NOT NULL,
+    CONSTRAINT [PK_User_UserID] PRIMARY KEY CLUSTERED (UserID ASC)
+)
