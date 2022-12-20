8ef3bec2ee7bfccdd85c08e550657bba72c535af
salehmu <ghd@keemail.me>
2022-12-20 06:00:21 +0200

    Refactor to Qaida

--- setup.sql
+++ setup.sql
@@ -1,49 +1,55 @@
 USE master;
+-- TODO add premision for each scheme
 
-IF DB_ID ('SIJL') IS NOT NULL DROP DATABASE SIJL;
+IF DB_ID('QAIDA') IS NOT NULL DROP DATABASE QAIDA;
 
 
 IF @@ERROR = 3702
-   RAISERROR ('Database cannot be dropped because there are still open connections.', 127, 127)
-   WITH NOWAIT, LOG;
+    RAISERROR ('Database cannot be dropped because there are still open connections.', 127, 127)
+        WITH NOWAIT, LOG;
 
 
-CREATE DATABASE SIJL;
-
+CREATE DATABASE QAIDA;
 GO
-USE SIJL;
+USE QAIDA;
 GO
 CREATE SCHEMA SIJL AUTHORIZATION dbo;
-
 GO
-CREATE TABLE SIJL.USERS (
-  id int IDENTITY (1, 1) NOT NULL,
-  hash BINARY (64) NOT NULL,
-  first_name varchar(30) NOT NULL,
-  last_name varchar(30) NOT NULL,
-  email varchar(254) NOT NULL UNIQUE,
-  username varchar(64) NOT NULL UNIQUE,
-  age smallint NOT NULL,
-  CONSTRAINT[PK_User_UserID] PRIMARY KEY CLUSTERED (id ASC))
+CREATE SCHEMA DISCUSS AUTHORIZATION dbo;
 GO
 
-ALTER TABLE SIJL.USERS
-  ADD github varchar (60) NULL;
+
+CREATE TABLE SIJL.USERS
+(
+    id            INT IDENTITY (1, 1) NOT NULL,
+    password_hash BINARY(64)          NOT NULL,
+    first_name    VARCHAR(30)         NOT NULL,
+    last_name     VARCHAR(30)         NOT NULL,
+    email         VARCHAR(254)        NOT NULL UNIQUE,
+    username      VARCHAR(64)         NOT NULL UNIQUE,
+    age           TINYINT             NOT NULL,
+    CONSTRAINT [PK_SIJL_UserID] PRIMARY KEY CLUSTERED (id ASC)
+)
 
 ALTER TABLE SIJL.USERS
-  ADD home VARCHAR(300) NULL;
+    ADD github varchar(60) NULL;
 
 ALTER TABLE SIJL.USERS
-  ADD about VARCHAR(1500) NULL;
+    ADD home VARCHAR(300) NULL;
 
 ALTER TABLE SIJL.USERS
-  ADD twitter VARCHAR(40) NULL;
+    ADD about NVARCHAR(1500) NULL;
+-- TODO check in the backend for size
 
 ALTER TABLE SIJL.USERS
-  ADD date_joined DATE NOT NULL DEFAULT '2022-12-13';
+    ADD twitter VARCHAR(40) NULL;
 
 ALTER TABLE SIJL.USERS
-  ADD avatar VARCHAR(10) NOT NULL DEFAULT 'empty';
+    ADD date_joined DATE NOT NULL DEFAULT '2022-12-13';
+
+-- TODO Implement avatars
+-- ALTER TABLE SIJL.USERS
+--     ADD avatar VARCHAR(10) NOT NULL DEFAULT 'empty';
 
 
 -- SELECT
